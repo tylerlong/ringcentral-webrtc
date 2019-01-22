af76af23cca6890c0c8e89c40c592516f1e6de9e
Tyler Long <tyler4long@gmail.com>
2019-01-22 11:10:29 +0800

    Add ringcentral-js-concise

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
+import 'babel-polyfill'
 import SIP from 'sip.js'
+import RingCentral from 'ringcentral-js-concise'
+
+const rc = new RingCentral(process.env.RINGCENTRAL_CLIENT_ID, process.env.RINGCENTRAL_CLIENT_SECRET, process.env.RINGCENTRAL_SERVER_URL)
+
+;(async () => {
+  await rc.authorize({
+    username: process.env.RINGCENTRAL_USERNAME,
+    extension: process.env.RINGCENTRAL_EXTENSION,
+    password: process.env.RINGCENTRAL_PASSWORD
+  })
+  const r = await rc.post('/restapi/v1.0/client-info/sip-provision', {
+    sipInfo: [{ transport: 'WSS' }]
+  })
+  console.log(r.data)
+})()
 
 const options = {
   media: {
